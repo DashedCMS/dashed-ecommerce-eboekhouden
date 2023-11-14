b5dbbf3aa84364059a10f5175187837fbdf949ac
Robin van Maasakker <robin@qubiqx.com>
2023-11-14 14:08:14 +0100

    Update

--- src/Filament/Widgets/EboekhoudenOrderStats.php
+++ src/Filament/Widgets/EboekhoudenOrderStats.php
@@ -11,9 +11,9 @@ class EboekhoudenOrderStats extends StatsOverviewWidget
     protected function getCards(): array
     {
         return [
-            Card::make('Aantal bestellingen naar E-boekhouden', EboekhoudenOrder::where('pushed', 1)->count()),
-            Card::make('Aantal bestellingen in de wacht', EboekhoudenOrder::where('pushed', 0)->count()),
-            Card::make('Aantal bestellingen gefaald', EboekhoudenOrder::where('pushed', 2)->count()),
+//            Card::make('Aantal bestellingen naar E-boekhouden', EboekhoudenOrder::where('pushed', 1)->count()),
+//            Card::make('Aantal bestellingen in de wacht', EboekhoudenOrder::where('pushed', 0)->count()),
+//            Card::make('Aantal bestellingen gefaald', EboekhoudenOrder::where('pushed', 2)->count()),
         ];
     }
 }
